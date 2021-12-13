dab9614bded80fc4ac3fff76b434025a0dd99ddc
Kenan Kadic <ken.kadic@outlook.com>
2021-12-13 10:17:05 -0600

    Searchbar.js added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
-function App() {
+const App = () => {
     return (
-        <div>
-        App    
+        <div className="ui container" style={{ marginTop: '10px'}}>
+            <SearchBar/>    
         </div>
-    )
-}
+    );
+};
 
 export default App
