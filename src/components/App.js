f5eecf10e12e9fc4c3c2cdc13e56ba0d83f98dcf
Kenan Kadic <ken.kadic@outlook.com>
2021-12-14 16:59:21 -0600

    api added

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import axios from 'axios';
+import React from 'react';
+import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    state = { images: [] };
+  state = { images: [] };
 
-    onSearchSubmit = async (term) => {
-      const response = await axios.get(' https://api.unsplash.com/search/photos', {
-           
-           params: { query: term },
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
+    });
 
-        });
+    this.setState({ images: response.data.results });
+  };
 
-        this.setState({ images: response.data.results })
-        }
-
-    render() {
+  render() {
     return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            <SearchBar onSubmit={this.onSearchSubmit}/>
-            Found : {this.state.images.length} images    
-        </div>
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
     );
   }
-};
+}
 
-export default App
+export default App;
+
