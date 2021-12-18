dfeb30965939f35996f02e564ced97ff36a1fbe6
Kenan Kadic <ken.kadic@outlook.com>
2021-12-18 14:56:22 -0600

    updates to css

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,31 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 class ImageCard extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.imageRef = React.createRef();
-    }
+    this.state = { spans: 0 };
 
-    componentDidMount(){
-        this.imageRef.current.addEventListener('load', 'this.setSpans');
-    }
+    this.imageRef = React.createRef();
+  }
 
-    setSpans = () => {
-        console.log(this.imageRef.current.ClientHeight);
-    }
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
 
-    render()  {
-        const { description, urls } = this.props.image;
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+
     return (
-        <div>
-            <img
-            ref={this.imageRef}
-            alt={description}
-            src={urls.regular} 
-            />       
-        </div>
-    )
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
 }
-}
-export default ImageCard
+
+export default ImageCard;
